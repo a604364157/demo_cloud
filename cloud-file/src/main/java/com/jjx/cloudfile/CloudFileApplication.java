3bb5bb784e04411e73ec8abdfb880f88a521ed40
jiangjx <640364157@qq.com>
2019-08-23 18:14:35 +0800

    初始化

---
+++ cloud-file/src/main/java/com/jjx/cloudfile/CloudFileApplication.java
@@ -0,0 +1,13 @@
+package com.jjx.cloudfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudFileApplication.class, args);
+    }
+
+}
