91c67827b0864df0356b58e1f6780ee6cea338b6
jiangjx <640364157@qq.com>
2019-09-29 15:38:54 +0800

    初始化

--- cloud-web/src/main/java/com/jjx/cloudweb/config/RequestMappingHandler.java
+++ cloud-web/src/main/java/com/jjx/cloudweb/config/RequestMappingHandler.java
@@ -5,6 +5,10 @@ import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 /**
+ * 重写RequestMappingHandlerMapping的isHandler
+ * 将接口或带feign的接口从mapping中排除
+ * 防止api包直接继承实现而导致的启动冲突
+ *
  * @author jiangjx
  */
 public class RequestMappingHandler extends RequestMappingHandlerMapping {
--- cloud-web/src/main/java/com/jjx/cloudweb/config/WebMvcConfiguration.java
+++ cloud-web/src/main/java/com/jjx/cloudweb/config/WebMvcConfiguration.java
@@ -1,16 +1,25 @@
 package com.jjx.cloudweb.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 /**
+ * web应用的统一配置类
+ * 用于对所有web工程的统一配置
+ * 如果工程有自己的特殊配置，则需要重写该类
+ * <p>
+ * 目前只重写了RequestMappingHandlerMapping
+ * 防止api的直接继承实现导致的启动冲突
+ *
  * @author jiangjx
  */
 @Configuration
 public class WebMvcConfiguration extends WebMvcConfigurationSupport {
 
     @Override
+    @NonNull
     protected RequestMappingHandlerMapping createRequestMappingHandlerMapping() {
         return new RequestMappingHandler();
     }
